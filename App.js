7879caf572598a6975231d08d7e64782ff69123d
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-27 21:14:12 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { View, BackHandler, Platform, } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  UNSAFE_componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <WebView
+          ref={(webView) => { this.webView.ref = webView; }}
+          onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+          automaticallyAdjustContentInsets={false}
+          source={{ uri: 'https://shisacomputer.herokuapp.com/' }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          style={{ marginTop: 25 }}
+        />
+      </View>
+    )
+  }
+
+}
